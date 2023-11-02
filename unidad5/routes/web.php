8fcaf2fb1817b255254e3b6b56aa3c61a578aeb9
Angel Almanza <almanza.angel245@gmail.com>
2023-11-02 01:50:31 -0700

    Trabajando con rutas GET

--- unidad5/routes/web.php
+++ unidad5/routes/web.php
@@ -13,6 +13,34 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::view('/', 'home')->name('home');
+
+Route::get('/operacion/{operacion}/{num1}/{num2}', function ($operacion, $num1, $num2) {
+    if (!is_numeric($num1) || !is_numeric($num2)) {
+        return view('error', ['message' => 'Parámetros no válidos']);
+    }
+
+    switch ($operacion) {
+        case 'suma':
+            $result = $num1 + $num2;
+            return view('operaciones', ['operation' => 'Suma', 'result' => $result]);
+        case 'resta':
+            $result = $num1 - $num2;
+            return view('operaciones', ['operation' => 'Resta', 'result' => $result]);
+        case 'division':
+            $result = $num1 / $num2;
+            return view('operaciones', ['operation' => 'Division', 'result' => $result]);
+        case 'multiplicacion':
+            $result = $num1 * $num2;
+            return view('operaciones', ['operation' => 'Multiplicacion', 'result' => $result]);
+        default:
+            return view('error', ['message' => 'Operación no válida']);
+    }
 });
+
+Route::get('/saludo/{name}/{lastname}', function ($name, $lastname) {
+    if (is_numeric($name) || is_numeric($lastname)) {
+        return view('error', ['message' => 'Parametros no validos']);
+    }
+    return view('greeting', ['name' => ucwords(strtolower($name)), 'lastname' => ucwords(strtolower($lastname))]);
+})->name('greeting');
